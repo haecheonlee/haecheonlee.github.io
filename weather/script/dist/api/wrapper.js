71b0f90e86c1f6973f32424785143dd31563b441
haecheonlee <haecheonlee@haecheonlees-MacBook-Pro.local>
2023-10-16 20:15:13 -0400

    Add extension

--- weather/script/dist/api/wrapper.js
+++ weather/script/dist/api/wrapper.js
@@ -1,51 +1,136 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, state, value, kind, f) {
-    if (kind === "m") throw new TypeError("Private method is not writable");
-    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a setter");
-    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot write private member to an object whose class did not declare it");
-    return (kind === "a" ? f.call(receiver, value) : f ? f.value = value : state.set(receiver, value)), value;
-};
-var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, state, kind, f) {
-    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a getter");
-    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
-    return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
-};
+var __awaiter =
+    (this && this.__awaiter) ||
+    function (thisArg, _arguments, P, generator) {
+        function adopt(value) {
+            return value instanceof P
+                ? value
+                : new P(function (resolve) {
+                      resolve(value);
+                  });
+        }
+        return new (P || (P = Promise))(function (resolve, reject) {
+            function fulfilled(value) {
+                try {
+                    step(generator.next(value));
+                } catch (e) {
+                    reject(e);
+                }
+            }
+            function rejected(value) {
+                try {
+                    step(generator["throw"](value));
+                } catch (e) {
+                    reject(e);
+                }
+            }
+            function step(result) {
+                result.done
+                    ? resolve(result.value)
+                    : adopt(result.value).then(fulfilled, rejected);
+            }
+            step(
+                (generator = generator.apply(thisArg, _arguments || [])).next()
+            );
+        });
+    };
+var __classPrivateFieldSet =
+    (this && this.__classPrivateFieldSet) ||
+    function (receiver, state, value, kind, f) {
+        if (kind === "m") throw new TypeError("Private method is not writable");
+        if (kind === "a" && !f)
+            throw new TypeError(
+                "Private accessor was defined without a setter"
+            );
+        if (
+            typeof state === "function"
+                ? receiver !== state || !f
+                : !state.has(receiver)
+        )
+            throw new TypeError(
+                "Cannot write private member to an object whose class did not declare it"
+            );
+        return (
+            kind === "a"
+                ? f.call(receiver, value)
+                : f
+                ? (f.value = value)
+                : state.set(receiver, value),
+            value
+        );
+    };
+var __classPrivateFieldGet =
+    (this && this.__classPrivateFieldGet) ||
+    function (receiver, state, kind, f) {
+        if (kind === "a" && !f)
+            throw new TypeError(
+                "Private accessor was defined without a getter"
+            );
+        if (
+            typeof state === "function"
+                ? receiver !== state || !f
+                : !state.has(receiver)
+        )
+            throw new TypeError(
+                "Cannot read private member from an object whose class did not declare it"
+            );
+        return kind === "m"
+            ? f
+            : kind === "a"
+            ? f.call(receiver)
+            : f
+            ? f.value
+            : state.get(receiver);
+    };
 var _Wrapper_api, _Wrapper_apiKey, _Wrapper_endpoints;
-import { config } from "../config/config";
+import { config } from "../config/config.js";
 export class Wrapper {
     constructor() {
         _Wrapper_api.set(this, void 0);
         _Wrapper_apiKey.set(this, void 0);
         _Wrapper_endpoints.set(this, void 0);
         __classPrivateFieldSet(this, _Wrapper_api, config.WEATHER_API, "f");
-        __classPrivateFieldSet(this, _Wrapper_apiKey, config.WEATHER_API_KEY, "f");
-        __classPrivateFieldSet(this, _Wrapper_endpoints, {
-            current: (options) => {
-                const { cityName, shouldGetAirQualityData } = options;
-                if (!cityName) {
-                    return null;
-                }
-                return {
-                    method: "GET",
-                    resource: `${__classPrivateFieldGet(this, _Wrapper_api, "f")}/current.json?key=${__classPrivateFieldGet(this, _Wrapper_apiKey, "f")}&q=${cityName}&aqi=${shouldGetAirQualityData}
+        __classPrivateFieldSet(
+            this,
+            _Wrapper_apiKey,
+            config.WEATHER_API_KEY,
+            "f"
+        );
+        __classPrivateFieldSet(
+            this,
+            _Wrapper_endpoints,
+            {
+                current: (options) => {
+                    const { cityName, shouldGetAirQualityData } = options;
+                    if (!cityName) {
+                        return null;
+                    }
+                    return {
+                        method: "GET",
+                        resource: `${__classPrivateFieldGet(
+                            this,
+                            _Wrapper_api,
+                            "f"
+                        )}/current.json?key=${__classPrivateFieldGet(
+                            this,
+                            _Wrapper_apiKey,
+                            "f"
+                        )}&q=${cityName}&aqi=${shouldGetAirQualityData}
                     `,
-                    params: {},
-                    body: {},
-                };
+                        params: {},
+                        body: {},
+                    };
+                },
             },
-        }, "f");
+            "f"
+        );
     }
     request(method, options) {
         return __awaiter(this, void 0, void 0, function* () {
-            const endpointCallback = __classPrivateFieldGet(this, _Wrapper_endpoints, "f")[method];
+            const endpointCallback = __classPrivateFieldGet(
+                this,
+                _Wrapper_endpoints,
+                "f"
+            )[method];
             if (!endpointCallback) {
                 return null;
             }
@@ -58,4 +143,6 @@ export class Wrapper {
         });
     }
 }
-_Wrapper_api = new WeakMap(), _Wrapper_apiKey = new WeakMap(), _Wrapper_endpoints = new WeakMap();
+(_Wrapper_api = new WeakMap()),
+    (_Wrapper_apiKey = new WeakMap()),
+    (_Wrapper_endpoints = new WeakMap());
--- weather/script/dist/script.js
+++ weather/script/dist/script.js
@@ -1,15 +1,41 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-import { Wrapper } from "./api/wrapper";
-import { Builder } from "./ui/builder";
-import { Utils } from "./util/utils";
+var __awaiter =
+    (this && this.__awaiter) ||
+    function (thisArg, _arguments, P, generator) {
+        function adopt(value) {
+            return value instanceof P
+                ? value
+                : new P(function (resolve) {
+                      resolve(value);
+                  });
+        }
+        return new (P || (P = Promise))(function (resolve, reject) {
+            function fulfilled(value) {
+                try {
+                    step(generator.next(value));
+                } catch (e) {
+                    reject(e);
+                }
+            }
+            function rejected(value) {
+                try {
+                    step(generator["throw"](value));
+                } catch (e) {
+                    reject(e);
+                }
+            }
+            function step(result) {
+                result.done
+                    ? resolve(result.value)
+                    : adopt(result.value).then(fulfilled, rejected);
+            }
+            step(
+                (generator = generator.apply(thisArg, _arguments || [])).next()
+            );
+        });
+    };
+import { Wrapper } from "./api/wrapper.js";
+import { Builder } from "./ui/builder.js";
+import { Utils } from "./util/utils.js";
 const toggleSpinner = (isSpinnerVisible) => {
     const spinner = document.getElementById("spinner");
     if (!spinner) {
@@ -17,8 +43,7 @@ const toggleSpinner = (isSpinnerVisible) => {
     }
     if (isSpinnerVisible) {
         spinner.classList.remove("hidden");
-    }
-    else {
+    } else {
         spinner.classList.add("hidden");
     }
 };
@@ -32,9 +57,11 @@ const bindOnInput = (id) => {
         const { length } = currentInput.value;
         if (length) {
             const validFontSize = (() => {
-                const computedFontSizeInRem = Number(window
-                    .getComputedStyle(document.documentElement)
-                    .getPropertyValue("--font-xl"));
+                const computedFontSizeInRem = Number(
+                    window
+                        .getComputedStyle(document.documentElement)
+                        .getPropertyValue("--font-xl")
+                );
                 if (!!computedFontSizeInRem && !isNaN(computedFontSizeInRem)) {
                     return computedFontSizeInRem;
                 }
@@ -42,43 +69,55 @@ const bindOnInput = (id) => {
             })();
             const calculatedWidth = length * validFontSize;
             currentInput.style.width = `${calculatedWidth}rem`;
-        }
-        else {
+        } else {
             currentInput.style.width = "";
         }
     };
-    const requestCurrentWeatherByCityName = (cityName) => __awaiter(void 0, void 0, void 0, function* () {
-        const wrapper = new Wrapper();
-        return yield wrapper.request("current", {
-            cityName,
-            shouldGetAirQualityData: "no",
+    const requestCurrentWeatherByCityName = (cityName) =>
+        __awaiter(void 0, void 0, void 0, function* () {
+            const wrapper = new Wrapper();
+            return yield wrapper.request("current", {
+                cityName,
+                shouldGetAirQualityData: "no",
+            });
         });
-    });
     input.oninput = (ev) => {
         const { target } = ev;
         if (target instanceof HTMLInputElement) {
             setInputWidthByContentLength(target);
             const { value } = target;
-            Utils.debounce(() => __awaiter(void 0, void 0, void 0, function* () {
-                const response = yield requestCurrentWeatherByCityName(value);
-                if (!response) {
-                    response;
-                }
-                else if ("error" in response) {
-                    Builder.renderWeatherTextForInvalidCity();
-                }
-                else {
-                    Builder.renderCurrentWeatherInCity(response.current.temp_c, response.current.condition.text, response.current.condition.icon, response.current.wind_mph, response.current.precip_mm);
-                }
-            }), () => {
-                Builder.resetCurrentWeather();
-                toggleSpinner(true);
-            }, () => {
-                if (!value) {
+            Utils.debounce(
+                () =>
+                    __awaiter(void 0, void 0, void 0, function* () {
+                        const response = yield requestCurrentWeatherByCityName(
+                            value
+                        );
+                        if (!response) {
+                            response;
+                        } else if ("error" in response) {
+                            Builder.renderWeatherTextForInvalidCity();
+                        } else {
+                            Builder.renderCurrentWeatherInCity(
+                                response.current.temp_c,
+                                response.current.condition.text,
+                                response.current.condition.icon,
+                                response.current.wind_mph,
+                                response.current.precip_mm
+                            );
+                        }
+                    }),
+                () => {
                     Builder.resetCurrentWeather();
-                }
-                toggleSpinner(false);
-            }, 2000);
+                    toggleSpinner(true);
+                },
+                () => {
+                    if (!value) {
+                        Builder.resetCurrentWeather();
+                    }
+                    toggleSpinner(false);
+                },
+                2000
+            );
         }
     };
 };
@@ -90,8 +129,7 @@ const toggleElementFocus = (id, shouldFocus) => {
     }
     if (shouldFocus) {
         element.focus();
-    }
-    else {
+    } else {
         element.blur();
     }
 };
